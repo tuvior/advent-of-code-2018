77ed90ddd55b865799165f5e7dce0a0a81fc7cb9
Tobias Bordenca <tobias.bordenca@epfl.ch>
2018-12-05 22:51:49 +0100

    Handle missing input file

--- src/main/kotlin/com/tuvior/adventofcode/day/Day.kt
+++ src/main/kotlin/com/tuvior/adventofcode/day/Day.kt
@@ -5,7 +5,9 @@ import com.tuvior.adventofcode.util.measureComputation
 abstract class Day<IN, R>(val n: Int, val title: String) : Runnable {
     private val inputLines: List<String> by lazy {
         val inputFilePath = "/day${"%02d".format(n)}.txt"
-        javaClass.getResource(inputFilePath).readText().lines().dropLastWhile { it.isEmpty() }
+        javaClass.getResource(inputFilePath)
+            ?.readText()?.lines()?.dropLastWhile { it.isEmpty() }
+            ?: throw IllegalStateException("Input file for Day $n doesn't exist. ($inputFilePath)")
     }
 
     protected abstract val inputTransform: (String) -> IN
