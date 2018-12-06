d3a49411e6f51fdbf44d2d82f9e8dd8ae0311fba
Tobias Bordenca <tobias.bordenca@epfl.ch>
2018-12-06 17:37:03 +0100

    Refactor day input type classes to not be inner classes

--- src/main/kotlin/com/tuvior/adventofcode/solutions/Day03.kt
+++ src/main/kotlin/com/tuvior/adventofcode/solutions/Day03.kt
@@ -2,7 +2,7 @@ package com.tuvior.adventofcode.solutions
 
 import com.tuvior.adventofcode.day.Day
 
-class Day03 : Day<Day03.Claim, Int>(3, "No Matter How You Slice It") {
+class Day03 : Day<Claim, Int>(3, "No Matter How You Slice It") {
 
     override val inputTransform: (String) -> Claim = Claim.Companion::parse
 
@@ -22,36 +22,36 @@ class Day03 : Day<Day03.Claim, Int>(3, "No Matter How You Slice It") {
     override fun solutionPart2(inputData: Sequence<Claim>): Int {
         return inputData.first { claim -> inputData.none { claim.id != it.id && claim overlaps it } }.id
     }
+}
 
-    class Claim(val id: Int, x: Int, y: Int, width: Int, height: Int) {
-        val xRange = x until x + width
-        val yRange = y until y + height
+class Claim(val id: Int, x: Int, y: Int, width: Int, height: Int) {
+    val xRange = x until x + width
+    val yRange = y until y + height
 
-        infix fun overlaps(otherClaim: Claim): Boolean {
-            if (xRange.first > otherClaim.xRange.last || otherClaim.xRange.first > xRange.last) {
-                return false
-            }
-
-            if (yRange.first > otherClaim.yRange.last || otherClaim.yRange.first > yRange.last) {
-                return false
-            }
-
-            return true
+    infix fun overlaps(otherClaim: Claim): Boolean {
+        if (xRange.first > otherClaim.xRange.last || otherClaim.xRange.first > xRange.last) {
+            return false
         }
 
-        companion object {
-            private val pattern = Regex("#(\\d+)\\s@\\s(\\d+),(\\d+):\\s(\\d+)x(\\d+)")
+        if (yRange.first > otherClaim.yRange.last || otherClaim.yRange.first > yRange.last) {
+            return false
+        }
 
-            fun parse(claim: String): Claim {
-                val result = pattern.matchEntire(claim)!!
-                return Claim(
-                    result.groupValues[1].toInt(),
-                    result.groupValues[2].toInt(),
-                    result.groupValues[3].toInt(),
-                    result.groupValues[4].toInt(),
-                    result.groupValues[5].toInt()
-                )
-            }
+        return true
+    }
+
+    companion object {
+        private val pattern = Regex("#(\\d+)\\s@\\s(\\d+),(\\d+):\\s(\\d+)x(\\d+)")
+
+        fun parse(claim: String): Claim {
+            val result = pattern.matchEntire(claim)!!
+            return Claim(
+                result.groupValues[1].toInt(),
+                result.groupValues[2].toInt(),
+                result.groupValues[3].toInt(),
+                result.groupValues[4].toInt(),
+                result.groupValues[5].toInt()
+            )
         }
     }
 }
--- src/main/kotlin/com/tuvior/adventofcode/solutions/Day04.kt
+++ src/main/kotlin/com/tuvior/adventofcode/solutions/Day04.kt
@@ -2,7 +2,7 @@ package com.tuvior.adventofcode.solutions
 
 import com.tuvior.adventofcode.day.Day
 
-class Day04 : Day<Day04.LogLine, Int>(4, "Repose Record") {
+class Day04 : Day<LogLine, Int>(4, "Repose Record") {
 
     override val inputTransform: (String) -> LogLine = LogLine.Companion::parse
 
@@ -45,55 +45,55 @@ class Day04 : Day<Day04.LogLine, Int>(4, "Repose Record") {
 
         return guardsTable
     }
+}
 
-    class LogLine(
-        private val year: Int,
-        private val month: Int,
-        private val day: Int,
-        private val hours: Int,
-        val minutes: Int,
-        content: String
-    ) : Comparable<LogLine> {
-        val logType = when {
-            "Guard" in content -> BEGIN_SHIFT
-            "falls asleep" == content -> FALL_ASLEEP
-            "wakes up" == content -> WAKE_UP
-            else -> OTHER
-        }
-        val guard = if (logType == BEGIN_SHIFT) content.split(" ")[1].drop(1).toInt() else -1
+class LogLine(
+    private val year: Int,
+    private val month: Int,
+    private val day: Int,
+    private val hours: Int,
+    val minutes: Int,
+    content: String
+) : Comparable<LogLine> {
+    val logType = when {
+        "Guard" in content -> BEGIN_SHIFT
+        "falls asleep" == content -> FALL_ASLEEP
+        "wakes up" == content -> WAKE_UP
+        else -> OTHER
+    }
+    val guard = if (logType == BEGIN_SHIFT) content.split(" ")[1].drop(1).toInt() else -1
 
-        override fun compareTo(other: LogLine): Int {
-            if (this == other) return 0
-            val yearC = year.compareTo(other.year)
-            if (yearC != 0) return yearC
-            val monthC = month.compareTo(other.month)
-            if (monthC != 0) return monthC
-            val dayC = day.compareTo(other.day)
-            if (dayC != 0) return dayC
-            val hourC = hours.compareTo(other.hours)
-            if (hourC != 0) return hourC
-            return minutes.compareTo(other.minutes)
-        }
+    override fun compareTo(other: LogLine): Int {
+        if (this == other) return 0
+        val yearC = year.compareTo(other.year)
+        if (yearC != 0) return yearC
+        val monthC = month.compareTo(other.month)
+        if (monthC != 0) return monthC
+        val dayC = day.compareTo(other.day)
+        if (dayC != 0) return dayC
+        val hourC = hours.compareTo(other.hours)
+        if (hourC != 0) return hourC
+        return minutes.compareTo(other.minutes)
+    }
 
-        companion object {
-            const val BEGIN_SHIFT = 0
-            const val FALL_ASLEEP = 1
-            const val WAKE_UP = 2
-            const val OTHER = 3
+    companion object {
+        const val BEGIN_SHIFT = 0
+        const val FALL_ASLEEP = 1
+        const val WAKE_UP = 2
+        const val OTHER = 3
 
-            private val pattern = Regex("\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})] (.*)")
+        private val pattern = Regex("\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})] (.*)")
 
-            fun parse(log: String): LogLine {
-                val result = pattern.matchEntire(log)!!
-                return LogLine(
-                    result.groupValues[1].toInt(),
-                    result.groupValues[2].toInt(),
-                    result.groupValues[3].toInt(),
-                    result.groupValues[4].toInt(),
-                    result.groupValues[5].toInt(),
-                    result.groupValues[6]
-                )
-            }
+        fun parse(log: String): LogLine {
+            val result = pattern.matchEntire(log)!!
+            return LogLine(
+                result.groupValues[1].toInt(),
+                result.groupValues[2].toInt(),
+                result.groupValues[3].toInt(),
+                result.groupValues[4].toInt(),
+                result.groupValues[5].toInt(),
+                result.groupValues[6]
+            )
         }
     }
 }
