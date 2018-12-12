7f184693a0ef8eab360bcf25a8a2c55bdd97daea
Tobias Bordenca <tobias.bordenca@epfl.ch>
2018-12-12 13:44:01 +0100

    Cleanup of reused code

--- src/main/kotlin/com/tuvior/adventofcode/solutions/Day12.kt
+++ src/main/kotlin/com/tuvior/adventofcode/solutions/Day12.kt
@@ -7,33 +7,15 @@ class Day12 : Day<String, Number>(12, "Subterranean Sustainability") {
     override val inputTransform: (String) -> String = { it }
 
     override fun solutionPart1(inputData: Sequence<String>): Int {
-        val initialState = inputData.first().removePrefix("initial state: ")
-            .mapIndexed { i, c -> i to c }
-            .filter { (_, c) -> c == '#' }
-            .map { (i, _) -> i }
-            .toSet()
-
-        val genStates = inputData.drop(2)
-            .map { line -> line.split(" => ").let { it[0] to it[1] } }
-            .filter { it.second == "#" }
-            .map { it.first }
-            .toSet()
+        val initialState = parseInitialState(inputData.first())
+        val genStates = parseGeneratingStates(inputData.drop(2))
 
         return generateSequence(initialState) { getNextGenerationState(it, genStates) }.take(21).last().sum()
     }
 
     override fun solutionPart2(inputData: Sequence<String>): Long {
-        val initialState = inputData.first().removePrefix("initial state: ")
-            .mapIndexed { i, c -> i to c }
-            .filter { (_, c) -> c == '#' }
-            .map { (i, _) -> i }
-            .toSet()
-
-        val genStates = inputData.drop(2)
-            .map { line -> line.split(" => ").let { it[0] to it[1] } }
-            .filter { it.second == "#" }
-            .map { it.first }
-            .toSet()
+        val initialState = parseInitialState(inputData.first())
+        val genStates = parseGeneratingStates(inputData.drop(2))
 
         val (divergenceStart, nextState) = generateSequence(initialState) { getNextGenerationState(it, genStates) }
             .mapIndexed { i, state ->
@@ -46,8 +28,22 @@ class Day12 : Day<String, Number>(12, "Subterranean Sustainability") {
         val (baseState, baseValue) = divergenceStart.first
         val constantInc = nextState.first.second - baseValue
 
-
         return baseValue + (50_000_000_000 - baseState) * constantInc
+    }
+
+    private fun parseInitialState(state: String): Set<Int> {
+        return state.removePrefix("initial state: ")
+            .mapIndexed { i, c -> i to c }
+            .filter { (_, c) -> c == '#' }
+            .map { (i, _) -> i }
+            .toSet()
+    }
+
+    private fun parseGeneratingStates(rules: Sequence<String>): Set<String> {
+        return rules.map { line -> line.split(" => ").let { it[0] to it[1] } }
+            .filter { it.second == "#" }
+            .map { it.first }
+            .toSet()
     }
 
     private fun getNextGenerationState(state: Set<Int>, genStates: Set<String>): Set<Int> {
